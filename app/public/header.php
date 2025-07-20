495a71384a9d2e2fc2ecb64aaebc5e8cf46549ae
Aidan Webster <contact@aidanwebster.uk>
2025-07-20 14:13:50 +0100

    Implement crude backend page support

---
+++ app/public/header.php
@@ -0,0 +1,42 @@
+<?php
+    include_once("../config.php");
+
+    $request = explode("/", $_SERVER['REQUEST_URI']);
+    $page = strtolower($request[1]);
+
+    if (!strlen($page)) {
+        $page = "home";
+    }
+
+    if (file_exists("pages/" . $page . ".php")) {
+?>
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
+    <title>Workout Planner - <?php echo ucfirst($page); ?></title>
+</head>
+<body>
+
+<?php
+    }
+    else {
+        http_response_code(404);
+?>
+
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
+    <title>Workout Planner - 404</title>
+</head>
+<body>
+        <h1>404 - Not Found</h1>
+        <p>The requested page does not exist on the server.</p>
+</body>
+</html>
+
+<?php
+        exit(1);
+    }
+?>
--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,25 @@
 <?php
-    phpinfo();
+    include_once("header.php");
+?>
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Test</li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+        <div id="content">
+            <?php
+                // We already checked if the page exists in header.php
+                include("pages/" . $page . ".php");
+            ?>
+        </div>
+    </main>
+    <?php echo $DB_HOST; ?>
+
+<?php
+    include_once("footer.php");
 ?>
